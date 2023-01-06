6377f803b2dd1f932d81b6273be3266b22d8ba69
Autumn Jencks <AutumnJencks@Autumns-MacBook-Pro-4.local>
2023-01-06 12:56:30 -0500

    I edited the histograms I made for homework and edited their bounds and continuity. Began looking at how to clean data

--- Data_Visualiser.R
+++ Data_Visualiser.R
@@ -8,14 +8,21 @@ par(mfrow=c(1,2))
 #Abundance Histogram and Plot
 abundance<-read.csv("./Raw Bird Data/SampleAbundance.csv")
 hist(abundance$Total,main="Species Abundance", ylab = "Frequency",xlab="Abundance")
-plot(x=abundance$Total,main="Species Abundance",ylab = "Frequency",xlab="Abundance")
+plot(x=abundance$Total,main="Species Abundance",y=rnorm(nrow(abundance),0,0.1),ylab = "",xlab="Abundance", yaxt="n")
 #Axes need to be flipped, frequency is on bottom and abundance is on y axis
 
+#Abundance Data Analysis
+mean(abundance$Total)
+# Total Mean = 167.97
+sd(abundance$Total)
+# Standard Deviation = 265.09
+
+#Biomass Histogram
 biomass<-read.csv("./Raw Bird Data/SampleBiomass.csv")
 hist(biomass$Total.mass,main="Biomass",ylab = "Frequency",breaks=50,xlab="Total Mass",xlim=c(0,5))
+#DISCOVERY--> Delete anything w/ negative note, handle negative mass values, recalculate ALL total mass
 
-#BodySize
+#Body Size Histograms
 bodysize<-read.csv("./Raw Bird Data/SampleBodySize.csv")
-hist(bodysize$Size.Class..5mm, xlim=c(0,300), breaks = 200, main="Frequency of Invertebrates under 5mm",xlab="Number of Invertebrates (<5mm)")
-
-hist(bodysize$Size.Class..5mm.1)
+hist(bodysize$Size.Class..5mm/bodysize$Total.Number, xlim=c(0,1),breaks=100, main="Proportion of Invertebrates under 5mm",xlab="Proportion of Invertebrates (<5mm)")
+hist(bodysize$Size.Class..5mm.1/bodysize$Total.Number, xlim=c(0,1),breaks=100, main="Proportion of Invertebrates Over 5mm",xlab="Proportion of Invertebrates (>5mm)")
