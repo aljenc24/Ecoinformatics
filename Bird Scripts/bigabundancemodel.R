12e4c0ca9461e3fc10c7749d3930ada0ca4e301b
Autumn Jencks <AutumnJencks@Autumns-MacBook-Pro-4.local>
2023-01-16 16:31:49 -0500

    created curve model to show the impact of solar day on taxa abundance for asparagus site

--- Bird Scripts/bigabundancemodel.R
+++ Bird Scripts/bigabundancemodel.R
@@ -95,7 +95,6 @@ curve(exp(coef.sol["(Intercept)"]+coef.sol["SolarDay"]*x), add = T, col="red")
 #Once again, red is the model, black is the data
 
 #Running a glm with squared term --> quadratic
-
 cleanedabundance$SqSolarDay<-cleanedabundance$SolarDay^2
 quad.model.sol<-glm(Total~SqSolarDay+SolarDay, data=cleanedabundance, family=poisson())
 summary(quad.model.sol)
@@ -114,8 +113,97 @@ summary(crazy.quad.site.model.sol)
 
 #To plot data only from one site: data= cleanedabundance[cleanedabundance$Site="Site Name"]
 
+# HOMEWORK GOAL: Make 8 plots for each site 
+
+png(height=11,width=7,pointsize=8,units="in",res=900,file="./Rough Figures/Site.Solar.Models.png")
+par(mfrow=c(4,2), mar=c(4,4,0.5,0.5))
+x.scale= c(160,260)
+#   1. Asparagus
+Asp.Abun=cleanedabundance[cleanedabundance$Site=="Asparagus",] 
+Asp.Abun.model<-glm(Total~SqSolarDay+SolarDay, data=Asp.Abun, family=poisson())
+coef.asp<-coef(Asp.Abun.model)
+plot(Total~SolarDay, data=Asp.Abun, xlim=x.scale, xlab="Solar Day", ylab="Total Abundance")
+curve(exp(coef.asp["(Intercept)"]+coef.asp["SolarDay"]*x+coef.asp["SqSolarDay"]*x^2), add = T, col="red")
+title(line=-3, "Asparagus")
+
+#   2. Barn Island
+Bar.Abun=cleanedabundance[cleanedabundance$Site=="Barn Island",] 
+Bar.Abun.model<-glm(Total~SqSolarDay+SolarDay, data=Bar.Abun, family=poisson())
+coef.bar<-coef(Bar.Abun.model)
+plot(Total~SolarDay, data=Bar.Abun, xlim=x.scale, xlab="Solar Day", ylab="Total Abundance")
+curve(exp(coef.bar["(Intercept)"]+coef.bar["SolarDay"]*x+coef.bar["SqSolarDay"]*x^2), add = T, col="red")
+title(line=-3, "Barn Island")
+
+#   3. Bunny
+Bun.Abun=cleanedabundance[cleanedabundance$Site=="Bunny",]  
+Bun.Abun.model<-glm(Total~SqSolarDay+SolarDay, data=Bun.Abun, family=poisson())
+coef.bun<-coef(Bun.Abun.model)
+plot(Total~SolarDay, data=Bun.Abun, xlim=x.scale)
+curve(exp(coef.bun["(Intercept)"]+coef.bun["SolarDay"]*x+coef.bun["SqSolarDay"]*x^2), add = T, col="red")
+title(line=-3, "Bunny")
+
+#   4. Chapman
+Cha.Abun=cleanedabundance[cleanedabundance$Site=="Chapman",] 
+Cha.Abun.model<-glm(Total~SqSolarDay+SolarDay, data=Cha.Abun, family=poisson())
+coef.cha<-coef(Cha.Abun.model)
+plot(Total~SolarDay, data=Cha.Abun, xlim=x.scale)
+curve(exp(coef.cha["(Intercept)"]+coef.cha["SolarDay"]*x+coef.cha["SqSolarDay"]*x^2), add = T, col="red")
+title(line=-3, "Chapman")
+
+#   5. Forsythe
+For.Abun=cleanedabundance[cleanedabundance$Site=="Forsythe",] 
+For.Abun.model<-glm(Total~SqSolarDay+SolarDay, data=For.Abun, family=poisson())
+coef.for<-coef(For.Abun.model)
+plot(Total~SolarDay, data=For.Abun, xlim=x.scale)
+curve(exp(coef.for["(Intercept)"]+coef.for["SolarDay"]*x+coef.for["SqSolarDay"]*x^2), add = T, col="red")
+title(line=-3, "Forsythe")
+
+#   6. Hammo
+Ham.Abun=cleanedabundance[cleanedabundance$Site=="Hammo",] 
+Ham.Abun.model<-glm(Total~SqSolarDay+SolarDay, data=Ham.Abun, family=poisson())
+coef.ham<-coef(Ham.Abun.model)
+plot(Total~SolarDay, data=Ham.Abun, xlim=x.scale)
+curve(exp(coef.ham["(Intercept)"]+coef.ham["SolarDay"]*x+coef.ham["SqSolarDay"]*x^2), add = T, col="red")
+title(line=-3, "Hammo")
+
+#   7. Rachel Carson
+Rac.Abun=cleanedabundance[cleanedabundance$Site=="Rachel Carson",] 
+Rac.Abun.model<-glm(Total~SqSolarDay+SolarDay, data=Rac.Abun, family=poisson())
+coef.rac<-coef(Rac.Abun.model)
+plot(Total~SolarDay, data=Rac.Abun, xlim=x.scale)
+curve(exp(coef.rac["(Intercept)"]+coef.rac["SolarDay"]*x+coef.rac["SqSolarDay"]*x^2), add = T, col="red")
+title(line=-3, "Rachel Carson")
+
+#   8. Sachuest
+Sac.Abun=cleanedabundance[cleanedabundance$Site=="Sachuest",] 
+Sac.Abun.model<-glm(Total~SqSolarDay+SolarDay, data=Sac.Abun, family=poisson())
+coef.sac<-coef(Sac.Abun.model)
+plot(Total~SolarDay, data=Sac.Abun, xlim=x.scale)
+curve(exp(coef.sac["(Intercept)"]+coef.sac["SolarDay"]*x+coef.sac["SqSolarDay"]*x^2), add = T, col="red")
+title(line=-3, "Sachuest")
+
+dev.off()
 
 
+
+## TRIAL>>> Abundance~Taxa for Asparagus site... 
+par(mfrow=c(1,2))
+
+#Orthoptera
+Asp.Abun=cleanedabundance[cleanedabundance$Site=="Asparagus",] 
+Asp.Orth.model<-glm(Orthoptera~SqSolarDay+SolarDay, data=Asp.Abun, family=poisson())
+coef.orth<-coef(Asp.Orth.model)
+plot(Orthoptera~SolarDay, data=Asp.Abun, xlab="Solar Day", ylab="Orthoptera Abundance")
+curve(exp(coef.orth["(Intercept)"]+coef.orth["SolarDay"]*x+coef.orth["SqSolarDay"]*x^2), add = T, col="red")
+
+#Hemiptera
+Asp.Abun=cleanedabundance[cleanedabundance$Site=="Asparagus",] 
+Asp.Hemi.model<-glm(Hemiptera~SqSolarDay+SolarDay, data=Asp.Abun, family=poisson())
+coef.hemi<-coef(Asp.Hemi.model)
+plot(Hemiptera~SolarDay, data=Asp.Abun, xlab="Solar Day", ylab="Orthoptera Abundance")
+curve(exp(coef.hemi["(Intercept)"]+coef.hemi["SolarDay"]*x+coef.hemi["SqSolarDay"]*x^2), add = T, col="red")
+
+#       ...
 
 
 
